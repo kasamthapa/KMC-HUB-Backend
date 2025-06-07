fabf3f31f9da5a011a2ddab43d2b1306a4003811
Kasam <kasamthapa89@gmail.com>
2025-06-07 20:01:38 +0545

    env added in gitignore

--- src/middlewares/studentAuth.ts
+++ src/middlewares/studentAuth.ts
@@ -2,7 +2,7 @@ import { NextFunction,Request,Response } from "express";
 import jwt from 'jsonwebtoken';
 
 export const studentAuth = async(req:Request,res:Response,next:NextFunction)=>{
-    try{
+
         const auth = req.headers['authorization'];
         if(!auth){
             res.status(403).json({
@@ -13,11 +13,11 @@ export const studentAuth = async(req:Request,res:Response,next:NextFunction)=>{
         try {
             const decoded = jwt.verify(auth, process.env.JWT_USER_SECRET_KEY) ;
             res.json({ message: 'Protected route accessed', user: decoded });
-        } catch (error) {
+        } catch (e) {
+            console.log('Erro verifying token',e);
             return res.status(403).json({ message: 'Invalid token' });
         }
 
     
-    }
    
 }
