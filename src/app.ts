c56d6c66d88599f8d3843cf626792830cf4e4e25
Kasam <kasamthapa89@gmail.com>
2025-06-09 23:12:11 +0545

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
-import express, { Application } from 'express';
-import {connectDB} from './config/DatabaseConnect';
-import userAuth from './features/auth/api/auth.routes'
-import postRoute from './features/collaboration_feed/api/posts.routes';
-const app:Application = express();
+import express, { Application } from "express";
+import cors from "cors";
+import { connectDB } from "./config/DatabaseConnect";
+import userAuth from "./features/auth/api/auth.routes";
+import postRoute from "./features/collaboration_feed/api/posts.routes";
+const app: Application = express();
 const port = 3000;
 connectDB();
 
+const allowedOrigins = "http://localhost:5173";
+// Configure CORS options
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin || "")) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"), false);
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/api/v1/auth',userAuth);
-app.use('/api/v1/posts',postRoute);
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use("/api/v1/auth", userAuth);
+app.use("/api/v1/posts", postRoute);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
